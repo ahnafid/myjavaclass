3824fc582fa13669e09cf18a5e4b4760fda0e505
ahnafid <arahmanamin123@gmail.com>
2019-04-10 01:33:15 -0400

    first try

---
+++ Dominos.java
@@ -0,0 +1,44 @@
+
+public class Dominos {
+	int em_id = 222;
+	String job_title = "driver";
+	String location = "crossbay";
+		
+	
+		
+		
+		
+		/*String name = "Aminur Rahman";
+		  int  dh = 8;
+		  int  phm = 15;
+		  
+		  int dailytotal = dh*phm;
+		  System.out.println("name=" +name+", daily income= $ "+dailytotal);
+	
+		  
+		  int   wh = 35;
+		  int weeklytotal = wh*phm;
+		  System.out.println("name="+name+ ",weekly income=$" +weeklytotal);
+		  
+		  int mh = 140;
+		  int monthlytotal = mh*phm;
+		  System.out.println("name="+name+ ",monthly income=$"+monthlytotal);
+		  
+		  int mh1 =140;
+		  int y =12;
+		  int yhrtotal= mh1*y;
+		  System.out.println("name"+name+ ",yearly hour="+yhrtotal);
+		  
+		  int yhr =1680;
+		  int yearlytotal = yhr*phm;
+		  System.out.println("name"+ ",yearly incme=$"+yearlytotal);
+		  */
+		  }
+	
+		
+	
+		
+	//}
+	
+	
+
